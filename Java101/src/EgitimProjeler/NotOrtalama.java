72895c4667979c68ee2c5ceb24be1a3b0b5b4821
Capsalt <hbasri2304@gmail.com>
2021-10-07 13:17:52 +0300

    ilk

--- Java101/src/EgitimProjeler/NotOrtalama.java
+++ Java101/src/EgitimProjeler/NotOrtalama.java
@@ -1,7 +1,29 @@
 package EgitimProjeler;
 
+import java.util.Scanner;
+
 public class NotOrtalama {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int mat, fiz, kim;
+
+        System.out.println("Matematik notunu giriniz:");
+        mat = scan.nextInt();
+        System.out.println("Fizik notunu giriniz:");
+        fiz = scan.nextInt();
+        System.out.println("Kimya notunu giriniz:");
+        kim = scan.nextInt();
+
+        int ort = (mat+fiz+kim)/3;
+
+        System.out.println("Ortalamanız: " + ort);
+
+        boolean gec = ort >=60;
+
+        if (gec){
+            System.out.println("Geçtiniz");
+        }else System.out.println("Kaldınız");
+
+
     }
 }
