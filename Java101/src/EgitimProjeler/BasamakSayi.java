7d22c226f6f125725be58e69b55e24dfc71f64ca
Capsalt <hbasri2304@gmail.com>
2021-10-15 16:44:13 +0300

    basamak

---
+++ Java101/src/EgitimProjeler/BasamakSayi.java
@@ -0,0 +1,24 @@
+package EgitimProjeler;
+
+import java.util.Scanner;
+
+public class BasamakSayi {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Girin: ");
+
+        int num = scan.nextInt();
+
+        int adet = 0,toplam=0;
+
+        while(num != 0)
+        {
+            toplam=(num%10)+toplam;
+            num /= 10;
+            ++adet;
+        }
+
+        System.out.println("Basamak Toplamı: " + toplam);
+    }
+}
